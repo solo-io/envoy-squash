1decb5ba74b092cf634e0a6294c6732da32e89a1
Yuval Kohavi <yuval.kohavi@gmail.com>
2017-12-08 19:17:58 -0500

    get the files needed from istio proxy.

--- build_docker.sh
+++ build_docker.sh
@@ -16,11 +16,19 @@ DEBUG_IMAGE_NAME=${DEBUG_IMAGE_NAME:-${REPO}/${PROJECT}/envoy-debug:${TAG}}
 
 BAZEL_TARGET_DIR="bazel-bin/"
 
-cp ../proxy/tools/deb/istio-iptables.sh ${BAZEL_TARGET_DIR}
-cp ../proxy/tools/deb/istio-start.sh ${BAZEL_TARGET_DIR}
-cp ../proxy/tools/deb/envoy.json ${BAZEL_TARGET_DIR}
-cp ../proxy/docker/proxy-* ${BAZEL_TARGET_DIR}
-cp ../proxy/docker/Dockerfile.debug ${BAZEL_TARGET_DIR}
+PROXY_GIT=https://github.com/istio/proxy proxy-tmp
+git clone PROXY_GIT
+cd proxy-tmp
+git checkout $PROXY_SHA
+cd ..
+
+FILE_PREFIX=proxy-tmp
+
+cp $FILE_PREFIX/tools/deb/istio-iptables.sh ${BAZEL_TARGET_DIR}
+cp $FILE_PREFIX/tools/deb/istio-start.sh ${BAZEL_TARGET_DIR}
+cp $FILE_PREFIX/tools/deb/envoy.json ${BAZEL_TARGET_DIR}
+cp $FILE_PREFIX/docker/proxy-* ${BAZEL_TARGET_DIR}
+cp $FILE_PREFIX/docker/Dockerfile.debug ${BAZEL_TARGET_DIR}
 docker build -f ${BAZEL_TARGET_DIR}/Dockerfile.debug -t "${DEBUG_IMAGE_NAME}" ${BAZEL_TARGET_DIR}
 
 
